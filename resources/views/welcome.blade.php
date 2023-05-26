311dea9cb1827c1d00d7e15da621ffed87aac4bd
kabid <azamat.kabiden@ffin.kz>
2023-05-26 23:01:46 +0600

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Test</title>
+        <title>Test 11</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
