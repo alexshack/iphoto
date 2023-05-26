28a819b5b8ecc475903bbd0183637ad358a00bc8
kabid <azamat.kabiden@ffin.kz>
2023-05-26 23:38:09 +0600

    Test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,7 +9,6 @@ use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class ApiController extends Controller
 {
-    protected $secret = '23fsefdsfsdf';
 
     public function handleWebhook(Request $request)
     {
@@ -46,49 +45,10 @@ class ApiController extends Controller
     {
 
         $githubtoken = $request->header('X-Hub-Signature');
-        $myTokenHash = 'sha1=' . hash_hmac('sha1',$request->getContent(), $this->secret);
+        $myTokenHash = 'sha1=' . hash_hmac('sha1',$request->getContent(), env('GITHUB_SECRET_KEY'));
         if($githubtoken != $myTokenHash) {
             return false;
         }
         return true;
-    }
-
-    public function deploy() {
-        if($this->checkGitHubHash()) {
-//            Log::info('git webhook checked');
-//            $process = new Process('sh '.env('DEV_AUTO_DEPLOY_SCRIPT_PATH'));
-//            $process->run();
-//
-//            if (!$process->isSuccessful()) {
-//                throw new ProcessFailedException($process);
-//            }
-//
-//            echo $process->getOutput();
-            exec('git pull origin dev');
-        }
-
-    }
-
-    public function checkGitHubHash() {
-        $payload = file_get_contents('php://input');
-//        if(!$this->verifyPatreonHash()) {
-//            return false;
-//        }
-        if ($payload['ref'] === 'refs/heads/dev') {
-            return false;
-        }
-        return true;
-    }
-
-    function verifyPatreonHash() {
-        $patreonBody = request()->getContent();
-        $patreonSignature = request()->header('X-Patreon-Signature');
-        $webhookHash = hash_hmac('md5', $patreonBody, $this->secret);
-
-        if(strtolower($webhookHash) == strtolower($patreonSignature)) {
-            return true; // Verification succeeded
-        } else {
-            return false; // Verification failed
-        }
     }
 }
