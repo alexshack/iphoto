9a08c0937abee40546be36986bac86031714adb4
kabid <azamat.kabiden@ffin.kz>
2023-05-26 19:17:15 +0600

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Test 11221111</title>
+        <title>Test 1</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
