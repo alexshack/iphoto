668c0c6bb3063210f0ce250ae6fa34fbe0011730
kabid <azamat.kabiden@ffin.kz>
2023-05-26 18:45:22 +0600

    Test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel 12343</title>
+        <title>Test</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
