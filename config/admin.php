dbcc324b5e8f8d5d3e2c5c06949e335953d847bf
kabid <azamat.kabiden@ffin.kz>
2023-07-17 08:12:59 +0600

    Фикс

--- config/admin.php
+++ config/admin.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 
 return [
-    'demo_admin_login' => env('demo_admin_login'),
-    'demo_admin_password' => env('demo_admin_password')
+    'demo_admin_login' => env('DEMO_ADMIN_LOGIN'),
+    'demo_admin_password' => env('DEMO_ADMIN_PASSWORD')
 ];
